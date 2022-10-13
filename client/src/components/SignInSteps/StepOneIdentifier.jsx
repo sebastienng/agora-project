1e0b47a370ff121dcabb3b418d9082084a089a7a
YUAN LIU <yuanliu0120@gmail.com>
2022-10-13 20:05:52 +0200

    feat(client) : formik install

---
+++ client/src/components/SignInSteps/StepOneIdentifier.jsx
@@ -0,0 +1,40 @@
+import "./StepOneIdentifier.css";
+import StepOneButton from "./StepOneButton";
+import freelance from "../../images/freelance.svg";
+import company from "../../images/company.svg";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
+
+function StepOneIdentifier({ setSignUpData }) {
+  const buttonHandler = (title) => {
+    setSignUpData((prevState) => {
+      return {
+        ...prevState,
+        type: title.toUpperCase(),
+      };
+    });
+  };
+
+  return (
+    <div className="stepOneContainer">
+      <h2>Let us know who you are...</h2>
+      <div className="buttonContainer">
+        <StepOneButton
+          img={freelance}
+          title={"Freelance"}
+          description={"I’m a freelancer looking for work"}
+          eventFn={buttonHandler}
+        />
+        <StepOneButton
+          img={company}
+          title={"Business"}
+          description={"I’m a Talent Acquisition looking for talents"}
+          eventFn={buttonHandler}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default StepOneIdentifier;
