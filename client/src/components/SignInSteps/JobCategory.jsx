6f91be1c487309eb1bcc4f58e8446e3732d2df57
Sebastien Niang <sebastien.niang@gmail.com>
2022-10-17 18:03:17 +0200

    feat(client) : possibility to select job category during step 3

--- client/src/components/SignInSteps/JobCategory.jsx
+++ client/src/components/SignInSteps/JobCategory.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { useState } from "react";
 import "./JobCategory.css";
 
-const JobCategory = ({ category, jobs, key }) => {
-  console.log(jobs);
+const JobCategory = ({ category, jobs }) => {
+  const [isActive, setActive] = useState();
+
+  function handleClick() {
+    setActive(!isActive);
+  }
   return (
-    <div className="job-category-box">
+    <div
+      className={isActive ? "job-category-box active" : "job-category-box"}
+      onClick={handleClick}
+    >
       <h3>{category}</h3>
       <span>{jobs.slice(0, 2).join(", ") + ", ..."}</span>
     </div>
