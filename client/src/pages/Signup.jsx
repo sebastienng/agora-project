4156f3ad8679ab4ef0b2d018d1508984f0c43ba4
Sebastien Niang <sebastien.niang@gmail.com>
2022-10-27 13:11:43 +0200

    fix

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -9,9 +9,8 @@ import StepSeven from "../components/SignInSteps/StepSeven";
 import StepThree from "../components/SignInSteps/StepThree";
 import StepEight from "../components/SignInSteps/StepEight";
 
-
 export default function Signup({ authenticate }) {
-  const [steps, setSteps] = useState(5);
+  const [steps, setSteps] = useState(1);
 
   const handleSteps = () => {
     setSteps(steps + 1);
@@ -20,27 +19,26 @@ export default function Signup({ authenticate }) {
   return (
     <div>
       {/* Charlotte changed the step order(1 and 2 ) , so we have change those comoents names later */}
-      {steps === 1 ? <StepTwo handleSteps={handleSteps} /> : null}
-      {steps === 2 ? <StepOneIdentifier handleSteps={handleSteps} /> : null}
-      {steps === 3 ? (
+      {steps === 1 && <StepTwo handleSteps={handleSteps} />}
+      {steps === 2 && <StepOneIdentifier handleSteps={handleSteps} />}
+      {steps === 3 && (
         <StepThree currentStep={steps} handleSteps={handleSteps} />
-      ) : null}
-      {steps === 4 ? (
+      )}
+      {steps === 4 && (
         <StepFour currentStep={steps} handleSteps={handleSteps} />
-      ) : null}
+      )}
+
+      {steps === 5 && (
         <StepFive currentStep={steps} handleSteps={handleSteps} />
-      {steps === 5 ? (
-      ) : null}
-      {steps === 6 ? (
-        <StepSix currentStep={steps} handleSteps={handleSteps} />
-      ) : null}
-      {steps === 7 ? (
+      )}
+      {steps === 6 && <StepSix currentStep={steps} handleSteps={handleSteps} />}
+      {steps === 7 && (
         <StepSeven currentStep={steps} handleSteps={handleSteps} />
-      ) : null}
-      {steps === 8 ? (
+      )}
+      {steps === 8 && (
         <StepEight handleSteps={handleSteps} currentStep={steps} />
-      ) : null}
-      {steps === 9 ? <StepNine handleSteps={handleSteps} /> : null}
+      )}
+      {/* {steps === 9 ? <StepNine handleSteps={handleSteps} /> : null} */}
     </div>
   );
 }
