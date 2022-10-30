8fad5f1d6a9f70145ba45182cac80282ce84a5cd
YUAN LIU <yuanliu0120@gmail.com>
2022-10-30 16:08:09 +0100

    feat(client): final css done

--- client/src/components/SignInSteps/FinalStep.jsx
+++ client/src/components/SignInSteps/FinalStep.jsx
@@ -3,7 +3,6 @@ import "./FinalStep.css";
 import MenuIcon from "../../images/MenuIcon.svg";
 import AGORA_LOGO from "../../images/logo-agora.png";
 import MessageIcon from "../../images/MessageIcon.svg";
-import NotificationIcon from "../../images/NotificationIcon.svg";
 import ProfileIcon from "../../images/ProfileIcon.svg";
 import BellIcon from "../../images/BellIcon.svg";
 
@@ -27,23 +26,23 @@ export default function FinalStep() {
             <img src={MessageIcon} alt="logo" />
           </li>
           <li>
-            <img src={NotificationIcon} alt="logo" />
+            <img src={ProfileIcon} alt="logo" />
           </li>
           <li>
-            <img src={ProfileIcon} alt="logo" />
+            <img src={BellIcon} alt="logo" />
           </li>
         </ul>
       </nav>
 
-      <div className="congratulationsContainer">
+      <div className="finalStepContentContainer">
         <h2>Congratulations!</h2>
         <p>
           Your profile has been created! We are matching your profile to
           companies’ missions.
         </p>
         <p>We’ll send you a notification when your matches are ready.</p>
-        <img src={BellIcon} alt="notification"/>
-        <p>In the meantime, you can have a look at your profile</p>
+        <img src={BellIcon} alt="notification" />
+        <p className="finalStepParagraphe">In the meantime, you can have a look at your profile</p>
       </div>
 
       <button className="agora-button">See my profile</button>
